61cb7c64a86d6d73946a3f00b6944ed30e70f054
Cami Rodriguez <camiepisode@outlook.com>
2016-11-05 01:21:35 -0500

    attempt to get temperature

--- RPI/app/controllers/socketController.js
+++ RPI/app/controllers/socketController.js
@@ -53,7 +53,7 @@ function Socket(dependencies){
         _humidityPort.on('open', function(){
             _console.log('Humidity Serial Port Opend', 'server-success');
             _humidityPort.on('data', function(data){
-                _console.log('Humidity:' + data[0], 'socket-message');
+                _console.log('Humidity:' + data[0] + ', Temperature:'+data[1], 'socket-message');
                 //_devFestServer.emit('DevFest.IoT.PI', {Command: 'HumidityPushData', Values:[{SensorData: data[0]}]} )
                 
             });
